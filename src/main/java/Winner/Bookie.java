710de8b73ba58916e3b3ce95e4abcdf615420f52
tushar singh <tushar.singh@lntinfotech.com>
2023-05-04 22:57:34 +0530

    First Commit

---
+++ src/main/java/Winner/Bookie.java
@@ -0,0 +1,37 @@
+package Winner;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Bookie {
+	
+	boolean placeBet(WebDriver driver, String side, double amount) {
+		try {
+		WebElement input = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div[1]/input"));
+		WebElement ct = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]/button"));
+		WebElement t = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div/div/div[1]/div[2]/div/div[5]/div[3]/button"));
+		if((amount) != 0) {
+			input.clear();
+			input.sendKeys(Double.toString(amount));
+			
+			if(side.equals("ct"))
+				ct.click();
+			else
+				t.click();
+			
+			input.clear();
+		}
+		
+			return false;
+		}catch(Exception e) {
+			driver.navigate().refresh();
+			
+			return true;
+		}
+	}	
+	
+
+}
---
+++ src/main/java/Winner/Start.java
@@ -0,0 +1,27 @@
+package Winner;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Start {
+
+	public static void main(String[] args) {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("--incognito");
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		Scanner sc = new Scanner(System.in);
+		driver.get("https://csgoempire.com");
+		System.out.println("5+3 :: Colateral = 1022");
+		System.out.println("Please Enter Your Capability : ");
+		double multiplier = sc.nextDouble();
+		Data data = new Data();
+		data.multiplier = multiplier;
+		new Algo().run(data, driver);
+	}
+
+}
